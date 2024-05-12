d05a06ba72d32d509f1bd0f161ec0e233de38c70
akhaled <akhaledlarus@gmail.com>
2024-05-12 03:21:29 +0300

    added request parser and response writer

---
+++ server/utils/http/response_writer.py
@@ -0,0 +1,48 @@
+class HTTPResponseWriter:
+    """
+    Provides a simple HTTP response writer that can be used to send responses to
+    clients connected to the server.
+
+    The `HTTPResponseWriter` class is responsible for constructing and sending
+    HTTP responses back to the client. It allows setting the status code, adding
+    headers, and setting the response content.
+    """
+    def __init__(self, client_socket):
+        self.client_socket = client_socket
+        self.status_code = 200
+        self.headers = {}
+        self.content = b""
+
+    def set_status_code(self, code):
+        self.status_code = code
+
+    def add_header(self, key, value):
+        self.headers[key] = value
+
+    def set_content(self, content):
+        self.content = content.encode('ascii')
+
+    def send_response(self):
+        """
+          Constructs the HTTP response header string with the status code and status message.
+
+          Args:
+              self (object): The current instance of the class.
+
+          Returns:
+              str: The HTTP response header string.
+          """
+        response = f"HTTP/1.1 {self.status_code} {self._get_status_message(self.status_code)}\r\n"
+        for key, value in self.headers.items():
+            response += f"{key}: {value}\r\n"
+        response += "\r\n"
+        response += self.content.decode()  
+        self.client_socket.sendall(response.encode('ascii'))
+
+    def _get_status_message(self, code):
+        # Maps status codes to messages (add more as needed)
+        messages = {
+            200: "OK",
+            404: "Not Found",
+        }
+        return messages.get(code, "Unknown Status Code")
