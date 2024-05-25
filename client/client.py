19ea786d4a2e667ba2655af3e8413eed34d8a248
akhaled <akhaledlarus@gmail.com>
2024-05-25 22:05:07 +0300

    hndled client exceptions

--- client/client.py
+++ client/client.py
@@ -1,3 +1,5 @@
+from src.http.request_creator import ClientHttpRequest
+from rich.console import Console
 from src.conn import ServerConn
 from rich.prompt import Prompt
 import sys
@@ -5,6 +7,18 @@ import sys
 try:
     username = Prompt.ask("[bold green]Enter your username")
     ServerConn(username)
+
+except ConnectionRefusedError:
+    Console().print("[bold red]Server is not running")
+    sys.exit(1)
+
 except KeyboardInterrupt:
-    print("\nGoodbye!")
+    if username:
+        # disconnect the client before ending
+        if ClientHttpRequest().POST(url="/disconn", host="localhost", port=9090, data={"client_name": username}).get_status_code() == 200:
+            Console().print(f"[bold white]\nGoodbye {username}!")
+        else:
+            Console().print(f"[bold red]\nerror disconnecting client")
+    else:
+        print("\nGoodbye!")
     sys.exit(0)
---
+++ client/src/format.py
@@ -0,0 +1,12 @@
+def format_headlines_json(json_data: dict) -> str:
+    """
+        `format_headlines_json` formats the json data to a string.
+        It takes in the json data and returns the formatted string.
+
+        Params:
+            json_data (dict): The json data to format.
+
+        Returns:
+            `str`: The formatted string.
+    """
+    
